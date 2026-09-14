c185cbbd3b0db7067211ac6c084f1ffc27b74f6c
Dulce Marbella Tolentino García <250917@utxicotepec.edu.mx>
2026-09-14 09:28:42 -0600

    Practica 1
    add: Se agrego la primera practica de la Unidad I, un ejemplo de Array en Python

---
+++ Prac-Unidad1/array.py
@@ -0,0 +1,13 @@
+# Crear un array (lista) de frutas
+frutas = ["manzana", "pera", "uva"]
+
+# Acceder al primer elemento
+print(frutas[0])  # Muestra: manzana
+
+# Modificar el segundo elemento
+frutas[1] = "naraja"
+print(frutas)     # Muestra: ['manzana', 'naraja', 'uva']
+
+# Agregar un elemento al final
+frutas.append("sandía")
+print(len(frutas)) # Muestra la cantidad de elementos: 4
