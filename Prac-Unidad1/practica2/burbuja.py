1697c469c93dc90034f6defa8a1ae0caf61ed2f1
Dulce Marbella Tolentino García <250917@utxicotepec.edu.mx>
2026-09-21 09:49:35 -0600

    Practica02
    add: se agrego el pdf de la actividad y se realizo la practica de orden ascendente y descendente

---
+++ Prac-Unidad1/practica2/burbuja.py
@@ -0,0 +1,10 @@
+lista = [2,3,6,8,10,7,8,9,4,5,10,3,4,8]
+n = len(lista)
+swapped = True
+while swapped:
+    swapped = False
+    for i in range(n-1):
+        if lista[i]>lista[i+1]:
+            lista[i],lista[i+1]=lista[i+1],lista[i]
+            swapped = True
+print("Lista Ordenada: ", lista)
